6503f682d4f2a0963cd2d7254cdcc37a9de57e23
karinesalbueno <karinebsalbueno@gmail.com>
2022-10-28 19:21:03 -0300

    feat: (morgan) HTTP request logger middleware for node.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require ('express');
+const morgan = require('morgan');
 const app = express();
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require ('./routes/pedidos');
+
+app.use(morgan('dev'));
 
 app.use('/produtos', rotaProdutos)
 app.use('/pedidos', rotaPedidos)
