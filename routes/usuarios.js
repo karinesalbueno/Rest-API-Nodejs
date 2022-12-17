0ef56db7e0e344680f90542ae5dc98ea58cbb619
Karine <karine.costa@somagrupo.com.br>
2022-12-17 15:17:00 -0300

    JWT token

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mysql = require('../mysql').pool
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 
 router.post('/cadastro', (req, res) => {
   mysql.getConnection((error, connection) => {
@@ -59,19 +60,26 @@ router.post('/login', (req, res) => {
           return res.status(500).send({ error: error })
         }
         if (result.length < 1) {
-          res
-            .status(400)
-            .send({ mensagem: 'falha na autenticação' })
-        } 
+          res.status(400).send({ mensagem: 'falha na autenticação' })
+        }
         bcrypt.compare(req.body.senha, result[0].senha, (error, result) => {
-            if(error){
-                return res.status(401).send({ mensagem: 'falha na autenticação' })
-            }
-            if (result){
-                return res.status(200).send({ mensagem: 'autenticado com sucesso!' })
-            }
+          if (error) {
             return res.status(401).send({ mensagem: 'falha na autenticação' })
-
+          }
+          if (result) {
+            const token = jwt.sign(
+              {
+                id_usuario: result.id_usuario,
+                email: result.email,
+              },
+              `${process.env.JWT_KEY}`,
+              { expiresIn: "2h" },
+            )
+            return res
+              .status(200)
+              .send({ mensagem: 'autenticado com sucesso!', token: token })
+          }
+          return res.status(401).send({ mensagem: 'falha na autenticação' })
         })
       },
     )
