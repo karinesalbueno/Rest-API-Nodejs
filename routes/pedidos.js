2aa179ec6f15a275fb6ceefed02111b75192f7e4
Karine <karine.costa@somagrupo.com.br>
2022-12-17 09:01:49 -0300

    rota pedidos com parâmetros

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,35 +1,90 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
+
 
 router.get('/', (req, res, next) =>{
-    res.status(200).send({
-        mensagem: 'Get pedidos'
-    });
+    mysql.getConnection((error, connection) => {
+        if (error) { return res.status(500).send({ error: error }) }
+
+        connection.query(
+            'SELECT * FROM pedidos',
+            (error, result) => {
+                if (error) { return res.status(500).send({ error: error }) }
+
+                const response = {
+                    quantidade: result.length,
+                    pedidos: result.map(ped => {
+                        return {
+                            id_pedido: ped.id_pedido,
+                            id_produto: ped.id_produto,
+                            quantidade: ped.quantidade,
+                        }
+                    })
+                }
+                return res.status(201).send(response)
+            }
+        )
+    })
 });
 
 router.post('/', (req, res, next)=>{
-    const pedido = {
-        id_produto: req.body.id_produto,
-        quantidade: req.body.quantidade
-    }
-    res.status(201).send({
-        mensagem: 'Post pedidos',
-        pedidoCriado: pedido
+    mysql.getConnection((error, connection) => {
+        connection.query(
+            'INSERT INTO pedidos (id_produto, quantidade) VALUES (?, ?)',
+            [req.body.id_produto, req.body.quantidade],
+
+            (error, result) => {
+                connection.release();
+
+                if (error) { return res.status(500).send({ error: error }) }
+
+                const response = {
+                    mensagem: 'Pedido criado com sucesso!',
+                    pedidoCriado: {
+                        id_pedido: result.id_pedido,
+                        id_produto: req.id_produto,
+                        quantidade: req.body.quantidade,
+                    }
+                }
+                res.status(201).send(response)
+            }
+        )
     })
+
 })
 
 router.get('/:id_pedido', (req, res, next) =>{
-    const id= req.params.id_produto
 
-    res.status(200).send({
-        mensagem: 'get por param pedidos',
-        id:id
-    });
+    mysql.getConnection((error, connection) => {
+        if (error) { return res.status(500).send({ error: error }) }
+
+        connection.query(
+            'SELECT * FROM pedidos WHERE id_pedido = ?',
+            [req.params.id_pedido],
+            (error, resultado) => {
+                if (error) { return res.status(500).send({ error: error }) }
+                return res.status(201).send({ response: resultado })
+            }
+        )
+    })
 });
 
 router.delete('/', (req, res, next)=>{
-    res.status(201).send({
-        mensagem: 'Delete pedidos'
+    mysql.getConnection((error, connection) => {
+        connection.query(
+            `DELETE from pedidos 
+                 WHERE id_pedidos = ?`,
+            [req.body.id_pedidos],
+
+            (error, resultado) => {
+                connection.release();
+                if (error) { return res.status(500).send({ error: error }) }
+                res.status(202).send({
+                    mensagem: 'Produto removido com sucesso!',
+                })
+            }
+        )
     })
 })
 
